4ec374af63a3345f3b5f994fe6c33a1bbb675b3e
Rama <berthinram@hotmail.com>
2018-11-11 02:17:49 -0800

    Create valentinog2

---
+++ valentinog2/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Hello world</h2>
+                <h3>Ths is my first react</h3>
+            </div>
+        );
+    }
+}
+export default App;
